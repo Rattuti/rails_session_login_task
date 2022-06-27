1e046a0b48bc215d7672a6f7a36022f11ad9f835
Rattuti <i.ishiharar.96296@gmail.com>
2022-06-27 23:54:31 +0900

    first

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    #user = User.find_by(email: params[:session][:email].downcase)
-    #if user&.authenticate(params[:session][:password])
-    #  session[:user_id] = user.id
-      #log_in user
-    #  redirect_to user_path(user.id)
-    #else
-    #  flash.now[:danger] = 'メールアドレスまたはパスワードに誤りがあります'
-    #  render :new
-    #end
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      log_in(user)
+      redirect_to user_path(user.id)
+    else
+      flash.now[:danger] = 'メールアドレスまたはパスワードに誤りがあります'
+      render :new
+    end
   end
 
   def destroy
