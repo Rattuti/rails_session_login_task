b8205921699b8a3edd25a75255764129f19eae3d
Rattuti <i.ishiharar.96296@gmail.com>
2022-06-29 10:45:09 +0900

    thrd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -45,10 +45,10 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:name, :email, password, :password_confirmation)
+        params.require(:user).permit(:name, :email, :password)
     end
 
     def current_user
-        
+
     end    
 end
